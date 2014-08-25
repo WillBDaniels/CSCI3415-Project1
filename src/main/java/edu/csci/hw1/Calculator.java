e86275ba4853e37c691b5970b3450d99b2cc3976
William Daniels <Willbdaniels@gmail.com>
2014-08-25 12:23:58 -0600

    This is the initial commit for the basic java calculator, includes the basic gui, some project setup for netbeans/gradle, things like that

---
+++ src/main/java/edu/csci/hw1/Calculator.java
@@ -0,0 +1,51 @@
+package edu.csci.hw1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * 08-2014 
+ * @author William
+ */
+public class Calculator extends Application {
+    private static Stage stage;
+    
+    
+    public static void main(String[] args) {
+        Application.launch(Calculator.class, args);
+    }
+    
+    /**
+     * Sets the stage for GUI(JavaFX). Style sheet is added in this method. This kicks
+     * off the primary FXML link and allows us to have the css added properly as well. 
+     *
+     * @param stage
+     * @throws Exception
+     */
+    @Override
+    public void start(final Stage stage) throws Exception {
+       
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/primary_window.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("css/primary.css").toExternalForm());
+        stage.setScene(scene);
+        stage.setTitle("Simple Java Calculator");
+        stage.resizableProperty().not();
+        stage.setResizable(false);
+        stage.centerOnScreen();
+        Calculator.stage = stage;
+        stage.show();
+
+      
+    }
+    
+    
+    public static Stage get_stage(){
+        return stage;
+    }
+}
+
---
+++ src/main/java/edu/csci/hw1/Primary_Controller.java
@@ -0,0 +1,19 @@
+package edu.csci.hw1;
+
+import javafx.fxml.FXML;
+
+/**
+ *
+ * 08-2014 
+ * @author William
+ */
+public class Primary_Controller {
+    
+    /**
+     * This is the main entry point into the calculator program .
+     */
+    @FXML
+    private void initialize(){
+        System.out.println("Properly initialized");
+    }
+}
