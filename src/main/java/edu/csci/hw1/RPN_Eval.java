5c41eeedc866e9c5950a040cdb1c306da5fdea96
zurawskj <josephzurawski@comcast.net>
2014-09-01 13:47:28 -0600

    Create RPN_Eval.java
    Class to solve an RPN expression.

---
+++ src/main/java/edu/csci/hw1/RPN_Eval.java
@@ -0,0 +1,27 @@
+package edu.csci.hw1;
+
+/*
+Author: ZURAWSKJ, 9-1-2014
+ This is the class that will handle solving an RPN expression.
+*/
+
+
+public class RPN_Eval 
+{
+  //CONSTRUCTOR
+  RPN_Eval(String expr_in)
+  {
+    rpn_string = expr_in;
+  }
+  
+  //ANSWER
+  //  PRE: The rpn_string data field contains a valid RPN expression.
+  //  POST: Returns a single solution to the RPN expression.
+  double answer()
+  {
+    
+  }
+  
+  private String rpn_string;
+
+}
