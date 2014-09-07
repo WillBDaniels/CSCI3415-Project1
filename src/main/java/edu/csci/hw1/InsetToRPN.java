1f890587e03cc69a355958e8a64775576eb4d5b4
shellhead <mike.hunsinger@gmail.com>
2014-09-07 14:10:39 -0600

    Added functionality for the InsetToRPN and added Operator class

--- src/main/java/edu/csci/hw1/InsetToRPN.java
+++ src/main/java/edu/csci/hw1/InsetToRPN.java
@@ -1,43 +1,101 @@
 package edu.csci.hw1;
 
+import java.util.Stack;
+
 /*
-Author: ZURAWSKJ, 9-1-2014
- This is the class that will handle converting inset to RPN.
+  Author: ZURAWSKJ, 9-1-2014
+  This is the class that will handle converting inset to RPN.
 */
 
+public class InsetToRPN {
+    private String inset_string;
+    private String rpn_string;
 
-public class InsetToRPN 
-{
-  //CONSTRUCTOR
-  //Receives a string in Inset Notation and converts to RPN
-  InsetToRPN(String expr_in)
-  {
-    inset_string = expr_in;
-  }
+    // CONSTRUCTOR
+    // Receives a string in Inset Notation and converts to RPN
+    public inset (String expr_in) {
+        inset_string = expr_in;
+    }
   
-  //ANSWER
-  //  PRE: The rpn_string data field contains a valid Inset Notation.
-  //  POST: Converts inset_string to RPN and stores in rpn_string.
-  void convert()
-  {
+    // ANSWER
+    // PRE: The rpn_string data field contains a valid Inset Notation.
+    // POST: Converts inset_string to RPN and stores in rpn_string.
+    public void convert() throws Exception {
+        CalculatorScanner cs = new CalculatorScanner(inset_string);
+        StringBuilder rpn = new StringBuilder();
+        Stack<Operator> operators = new Stack<Operator>();
+
+        try {
+            // read the entire expression
+            while (cs.hasNextToken()) {
+                String token = cs.readToken();
+
+                // if its a number, it can go on the string
+                if (isDouble(token)) {
+                    rpn.append(token + " ");
+                } else {
+                    Operator curr = new Operator(token);
+
+                    // pop existing operators if they have higher precedence 
+                    if (!operators.empty() && !curr.toString().equals("(")) {
+                        Operator top = operators.peek();
+
+                        while ((top.getPrecedence() >= curr.getPrecedence())
+                               && !operators.empty() && curr.getLeftAssoc()) {
+                            top = operators.pop();
+                            rpn.append(top.toString() + " ");
+
+                            // look at next operator
+                            top = operators.peek();
+                        }
+                    }
+
+                    // see if there's a matching left parenthesis
+                    if (curr.toString().equals(")")) {
+                        for (Operator next = operators.pop();
+                             !next.toString().equals("(");
+                             next = operators.pop()) {
+                            rpn.append(next.toString() + " ");
+                        }
+                    }
+                    
+                    if (!curr.toString().equals(")")) {
+                        operators.push(curr);
+                    }
+                }
+            }
+
+            while (!operators.empty()) {
+                rpn.append(operators.pop().toString() + " ");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        rpn_string = rpn.toString();
+    }
     
-  }
-  
-  //GET_INSET_STRING
-  //  POST: Returns inset_string
-  String getInsetString()
-  {
-    return inset_string;
-  }
-  
-  //GET_RPN_STRING
-  //POST: Returns RPN_string
-  String getRPNString()
-  {
-    return rpn_string;
-  }
-  
-  private String inset_string;
-  private String rpn_string;
+    private boolean isDouble(String str) {
+        try {
+            Double.parseDouble(str);
 
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    // GET_INSET_STRING
+    // POST: Returns inset_string
+    public String getInsetString()
+    {
+        return inset_string;
+    }
+  
+    // GET_RPN_STRING
+    // POST: Returns RPN_string
+    public String getRPNString()
+    {
+        return rpn_string;
+    }
 }
---
+++ src/main/java/edu/csci/hw1/Operator.java
@@ -0,0 +1,53 @@
+package edu.csci.hw1;
+
+public class Operator {
+    private int precedence;
+    private String symbol;
+    private boolean leftAssoc; 
+
+    public Operator(String op) {
+        setPrecedence(op);
+        symbol = op;
+        leftAssoc = op.equals("^") ? false : true;
+    }
+
+    private void setPrecedence(String op) {
+        switch (op) {
+        case "+":
+        case "-":
+            precedence = 1;
+            break;
+        case "/":
+        case "*":
+            precedence = 2;
+            break;
+        case "^":
+            precedence = 3;
+            break;
+        case "(":
+            precedence = 0;
+            break;
+        case ")":
+            precedence = 4;
+            break;
+        default:
+            break;
+        }
+    }
+
+    public int getPrecedence() {
+        return precedence;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public boolean getLeftAssoc() {
+        return leftAssoc;
+    }
+
+    public String toString() {
+        return symbol;
+    }
+}
