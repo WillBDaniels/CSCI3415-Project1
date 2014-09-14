fd36ae0fafbb693184ea36767aa7bb8618af20ae
shellhead <mike.hunsinger@gmail.com>
2014-09-14 14:01:33 -0600

    Fi

--- src/main/java/edu/csci/hw1/InsetToRPN.java
+++ src/main/java/edu/csci/hw1/InsetToRPN.java
@@ -23,6 +23,8 @@ public class InsetToRPN {
                                    .replaceAll("\\+-", "-")
                                    .replaceAll("-\\+", "-")
                                    .replaceAll("\\+\\+", "+")
+                                   .replaceAll("\\*\\+", "*")
+                                   .replaceAll("\\/\\+", "/")
                                    .replaceAll("\\*\\-", "*!")
                                    .replaceAll("\\/\\-", "/!")
                                    .replaceAll("\\^\\-", "^!")
